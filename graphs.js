a148f87c119a3621bfefa9caff8774953fb2e08e
louis-desenv <128602112+louis-desenv@users.noreply.github.com>
2024-09-24 00:49:15 -0300

    slide 9

---
+++ graphs.js
@@ -0,0 +1,48 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Access the grid-container div
+    
+    const gridContainer = document.querySelector('.grid-container');
+    const gridContainerBottom = document.querySelector('.grid-container-bottom');
+
+    // Check if gridContainer is not null
+    if (gridContainer) {
+        // Create columns dynamically
+        
+        for (let i = 0; i < 17; i++) { 
+           
+            const columnDiv = document.createElement('div');
+            columnDiv.classList.add('column');
+           
+            const itemUpDiv = document.createElement('div');
+            itemUpDiv.classList.add('item-up');
+            itemUpDiv.textContent = '1'; // Add content to item-up
+            itemUpDiv.style.height =Math.floor(Math.random() * 100+12)+ "px" ;           
+            columnDiv.appendChild(itemUpDiv);
+
+            gridContainer.appendChild(columnDiv);
+        }
+    } else {
+        console.error("grid-container element not found");
+    }
+
+     // Check if gridContainerBottom is not null
+     if (gridContainerBottom) {
+        // Create columns dynamically
+        
+        for (let i = 0; i < 17; i++) { 
+           
+            const columnDiv = document.createElement('div');
+            columnDiv.classList.add('column');
+
+            const itemDownDiv = document.createElement('div');
+            itemDownDiv.classList.add('item-down');
+            itemDownDiv.textContent = '2'; // Add content to item-down
+            itemDownDiv.style.height =Math.floor(Math.random() * 100 +12) + "px" ;            
+            itemDownDiv.style.transform = 'rotate(180deg)';
+           columnDiv.appendChild(itemDownDiv);            
+            gridContainerBottom.appendChild(columnDiv);
+        }
+    } else {
+        console.error("grid-container-bottom element not found");
+    }
+});
